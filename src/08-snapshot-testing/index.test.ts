1e4d0f85a41fed9bf697211584ef0eba0f6ab91c
nowaylifer <nowaylifer@gmail.com>
2024-02-07 16:52:09 +0300

    feat: complete 5-8)

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toStrictEqual({
+      value: 1,
+      next: { value: 2, next: { value: 3, next: { value: null, next: null } } },
+    });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toMatchInlineSnapshot(`
+      {
+        "next": {
+          "next": {
+            "next": {
+              "next": null,
+              "value": null,
+            },
+            "value": 3,
+          },
+          "value": 2,
+        },
+        "value": 1,
+      }
+    `);
   });
 });
